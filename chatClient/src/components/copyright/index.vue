a1b26b54c95593d9f4d18432aabf7e3a5dac1213
llt <2450672562@qq.com>
2023-03-30 14:48:01 +0800

    submmit chatSys

---
+++ chatClient/src/components/copyright/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="copyright-cmp">
+    <p class="copyright-item">Copyright©2023-{{endTime}}</p>
+    <p class="copyright-item">作者：llt</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      endTime: new Date().getFullYear()
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.copyright-cmp {
+  position: fixed;
+  bottom: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  .copyright-item {
+    margin: 0 0 5px;
+    color: #bbb;
+    font-size: 14px;
+  }
+}
+</style>
